688f59a9f5de8f152175783e21e8fe10abcbf29d
neehal <112001034@smail.iitpkd.ac.in>
2023-06-05 06:51:36 +0400

    simple code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ridingthechatgptwave
+
+
+# HELLO!!!
+I am Rapeti Siddhu Neehal and I am glad you are interested in my audiobook converter chatgot project.
+the code I wrote uses the chatgpt api (engine: 'text-davinci-003')
+
+My code can generate the audio file and the text summary of the book with the help of chatgpt api and gtts. 
+## The basic outline:
+* the libraries I utitlized are streamlit, gTTS(google text-to-speech), requests and os.
+* The web app takes 2 inputs from the users: 1) book name, and 2) word limit of the summary
---
+++ audiobook_converter.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import requests
+from gtts import gTTS
+import os
+
+# Function to convert text to speech and save it as an audio file
+# For this task, I am using the gTTS library
+
+def text_to_speech(text, filename):
+    tts = gTTS(text=text, lang='en', tld='com.mx')
+    tts.save(filename)
+
+
+# Set up the Streamlit app
+st.title("Book Summary to Audio Converter")
+
+# Create a form for the user to input the book name and the word size required
+book_name = st.text_input("Enter the name of the book:")
+word_size = st.slider("Select the word size required:", min_value=50, max_value=500, step=25)
+
+st.write(api_key)
+# Create a button to generate the audio file
+if st.button("Generate Audio"):
+    if api_key:
+        # Make a request to the ChatGPT API to get a summary of the book
+        url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}"
+            # Get the API key from an environment variable ___this will be the part that you will have to change___
+        }
+        data = {
+            "prompt": f"Summarize the book '{book_name}' in approximately {word_size} words. Start with the author name and the year the book was published as a sentence, and then continue to summarize the book in the writing style of the author.",
+            "max_tokens": word_size,
+            "temperature": 1,
+        }
+        response = requests.post(url, headers=headers, json=data)
+        st.write(response.json())
+        summary = response.json()["choices"][0]["text"]
+
+        summary = summary.replace('"', '')
+        summary = summary.replace('\'', '')
+
+        st.write(summary)
+        # Convert the summary to an audio file
+        output_filename = "audiobook.mp3"
+        text_to_speech(summary, output_filename)
+
+        # Make the audio file available for download
+        st.audio(output_filename, format="audio/mp3")
+        # Add a download button for the audio file
+        with open(output_filename, 'rb') as file:
+            st.download_button("Download Audio", file.read(), file_name='audiobook.mp3')
+    else:
+        st.error("API key not found. Please make sure to set the OPENAI_API_KEY environment variable.")
